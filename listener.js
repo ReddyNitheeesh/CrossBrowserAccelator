a6992f5d3697ff55ea57b6701a17c1a00ad4879c
Manohar_Gadepally <Manohar_Gadepally@epam.com>
2023-01-15 13:22:06 +0530

    Added extension which logs id into console

--- listener.js
+++ listener.js
@@ -1,10 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const { ServiceBuilder } = require('selenium-webdriver/chrome');
+const {Builder, By, Key, until, Browser, Options, WebDriver} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
-const chromeDriverPath = ".\\drivers\\chromedriver.exe"; // or wherever you've your driver path is.
-const serviceBuilder = new ServiceBuilder(chromeDriverPath);
+// const chromeDriverPath = ".\\drivers\\chromedriver.exe"; // or wherever you've your driver path is.
+// const serviceBuilder = new chrome(chromeDriverPath);
 const ids = [];
 
 window.addEventListener('input',(e)=>{
@@ -19,26 +19,23 @@ window.addEventListener('click',(e)=>{
     var elementId = e.target.id;
     if(elementId) {
         console.log(elementId);
-        example();
+        var chromeOptions = new chrome.Options();
+        chromeOptions.addArguments(`--load-extension=C:\\Manohar\\ExtensionClick`);
+        let driver = new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(chromeOptions)
+        .build();
+        example(driver);
     }
 })
 
-async function example() {
-    document.addEventListener('input',(e)=>{
-        var input=e.target
-        console.log(input.id)
-    })
-    const driver = new Builder()
-    .forBrowser('chrome')
-    .setChromeService(serviceBuilder)
-    .build();
+async function example(driver) {
     try {
         await driver.get('http://www.google.com/ncr');
         await driver.findElement(By.name('q')).sendKeys('webdriver');
-        // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+        
     } finally {
         // await driver.quit();
     }
 }
-
-
