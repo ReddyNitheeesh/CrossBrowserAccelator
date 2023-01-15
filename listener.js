5f17a224a9e98e1bb03b2ebd1673ae52fd0781e5
Manohar_Gadepally <Manohar_Gadepally@epam.com>
2023-01-15 14:01:19 +0530

    Removed unwanted code

--- listener.js
+++ listener.js
@@ -3,28 +3,15 @@ const path = require('path');
 const {Builder, By, Key, until, Browser, Options, WebDriver} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-// const chromeDriverPath = ".\\drivers\\chromedriver.exe"; // or wherever you've your driver path is.
-// const serviceBuilder = new chrome(chromeDriverPath);
-const ids = [];
-
-window.addEventListener('input',(e)=>{
-    var input=e.target
-    console.log(input.id)
-    // console.log(e)
-    // console.log(e.target)
-    // console.log(e.data)
-})
-
 window.addEventListener('click',(e)=>{
     var elementId = e.target.id;
     if(elementId) {
-        console.log(elementId);
         var chromeOptions = new chrome.Options();
         var loadExtension = '--load-extension=';
         var globalRoot = require('path').resolve('./');
         var myExtension = 'my_extension';
         let result = loadExtension.concat(globalRoot, "\\", myExtension);
-        console.log(result);
+
         chromeOptions.addArguments(result);
         let driver = new Builder()
         .forBrowser('chrome')
